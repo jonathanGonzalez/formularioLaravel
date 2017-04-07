3381f586ba6d0c0da3e890cd6a9ba04f92bf99c0
jonathanGonzalez <jota_q91@hotmail.com>
2017-04-07 00:05:04 -0500

    formulario finalizado con html & forms de laravel

--- resources/views/formularios/restaurante.blade.php
+++ resources/views/formularios/restaurante.blade.php
@@ -5,12 +5,71 @@
 @stop
 @section('contenido')   
     {!! Form::open(['url' => 'foo/bar']) !!}
+    
+    {!! Form::label('p1_lbl','¿volvería a nuestro restaurante?');!!}       
+    {!! Form::label('p1_lbl','si');!!}       
+    {!! Form::radio('name', 'value', true); !!}
+    {!! Form::label('p1_lbl','no');!!}           
+    {!! Form::radio('name', 'value', false); !!}
+
+    <br>
+    <br>
+    <br>
+
+    {!! Form::label('positivo_lbl', '¿Qué fue lo mejor de su visita a nuestro restaurante?'); !!}
+    {!! Form::textarea('email_txt', '', ['class' => 'form-control']); !!}
+
+    <br>
+
+    {!! Form::label('positivo_lbl', '¿Qué no le gusto del restaurante?'); !!}
+    {!! Form::textarea('email_txt', '', ['class' => 'form-control']); !!}
+
+    <br>
+    <br>
+
+    {!! Form::label('p1_lbl','Desea recibir correos con información de promociones?');!!}       
+    {!! Form::label('p1_lbl','si');!!}       
+    {!! Form::radio('name', 'value', true); !!}
+    {!! Form::label('p1_lbl','no');!!}           
+    {!! Form::radio('name', 'value', false); !!}
+
+    <br>
+    <br>
 
     {!! Form::label('email_lbl', 'E-Mail Address:'); !!}
     {!! Form::text('email_txt', 'example@gmail.com', ['class' => 'form-control']); !!}
+
     <br>
-    {!! Form::label('Fecha_lbl', 'Fecha de nacimiento', ['class' => 'form-control']); !!}
+    <br>
+
+    {!! Form::label('Fecha_lbl', 'Cuándo fué su última visita:'); !!}
     {!! Form::date('fecha_date', \Carbon\Carbon::now()); !!}
+
+    <br>
+    <br>
+    
+    {!! Form::label('Fecha_lbl', 'Qué fué lo que más le gusto del restaurante:'); !!}
+    {!! Form::select('size', ['c' => 'La comida', 'S' => 'El servicio', 'n' => 'nada'], 'S'); !!}
+
+    <br>
+    <br>
+    
+    {!! Form::label('Fecha_lbl', 'Que prefiere en un restaurantes de comida rapidas'); !!}
+    {!! Form::select('size', ['t' => 'tiempos cortos', 'p' => 'buena presentación de la comida', 'las dos' => 'nada'], 'S'); !!}
+
+    <br>
+    <br>    
+
+    {!! Form::label('ha leido los terminos y condiciones', '');!!}
+    {!! Form::checkbox('name', 'value', true); !!}
+
+    <br>
+    <br>
+    
+    {!! Form::submit('enviar encuesta', ['class' => 'btn btn-primary'])!!}
+
+    <br>
+    <br>
         
     {!! Form::close() !!}    
 @stop
